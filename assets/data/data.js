22eb891e01c521a2ea6077d4661f6234b245c14d
bieito.job <bieito.sousa.1991@gmail.com>
2022-12-28 10:56:54 +0100

    Proyects - LaSal

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,251 +1,306 @@
 const data = {
-
-    "aboutMe": {
-        "name": "Conóceme",
-        "description": "Desarrollador de aplicaciones web y móvil.<br/>Con formación en Front y Back, capacidad de trabajo en grupo mediante el uso de tecnologías ágiles.<br/>Adaptabilidad a diferentes lenguajes de programación y arquitecturas de diseño.",
-        "descAccent": "Estaría encantado de formar parte de su empresa, y seguir desarrollándome en el ámbito profesional.",
-        "cvHref": "CV_Bieito.pdf",
-        "downloadMSN": "Descarga mi CV"
-    },
-    //   -  - Experiencia - - -
-    "experience": {
-        "name": "Experiencia",
-        "docs": [
-            {
-                "year": "2022",
-                "duration": "1 año",
-                "name": "Galvintec (Diciembre 2021 - Diciembre 2022)",
-                "job": "Desarrollador Aplicaciones Android.",
-                "performance": "Maquetar y crear funcionalidades a medida en entornos Android.",
-                "technologies": "Android Studio, Kotlin, ReactNative…",
-            },
-            {
-                "year": "2021",
-                "duration": "3 meses",
-                "name": "Galvintec (Septiembre 2021 - Diciembre 2021)",
-                "job": "Desarrollador Aplicaciones Web.",
-                "performance": "Creación web e-commerce.",
-                "technologies": "NextJS, React, Nodejs…",
-            },
-            {
-                "year": "2021",
-                "duration": "6 meses",
-                "name": "IMATIA (Septiembre 2020 - Marzo 2021)",
-                "job": "Desarrollador Web.",
-                "performance": "Creación de una web.",
-                "technologies": "SQL, Java, Spring, Angular…",
-            },
-            {
-                "year": "2019",
-                "duration": "3 meses",
-                "name": "TOKAPP SCHOOL (Septiembre 2019 - Diciembre 2019)",
-                "job": "Desarrollador Web.  (Prácticas) ",
-                "performance": "Creación de nuevas funcionalidades para web.",
-                "technologies": "PHP, JavaScript, SQL, WordPress, Moodle...",
-            },
-            {
-                "year": "2018",
-                "duration": "6 meses",
-                "name": "LIDERA KNOWLEDGE SL (Diciembre 2018 - Mayo 2019)",
-                "job": "Técnico de Sistemas Informáticos en red.",
-                "performance": "Creación y mantenimiento de un servidor web, destinado al E-Learning",
-                "technologies": "CSM,  WordPress, Moodle",
-            },
-            {
-                "year": "2017",
-                "duration": "6 meses",
-                "name": "SATEC (Marzo 2017 - Agosto 2017)",
-                "job": "Gestor de integración",
-                "performance": "Configuración, gestión y asistencia de incidencias para rúters.",
-                "technologies": "Instalacion y configuración en remoto de Terminales (Cisco, Huawei, Teldat...), Gestion de servicios (VPN, firewall...)",
-            },
-            {
-                "year": "2016",
-                "duration": "3 meses",
-                "name": "SERESCO (Septiembre 2016 - Diciembre 2016)",
-                "job": "Técnico de Sistemas Informáticos en red. (Practicas)",
-                "performance": "Gestión y mantenimiento de equipos en remoto (seguridad informática, asesoramiento de las infraestructuras informáticas, prevención de riesgos informáticos, copias de seguridad y tratamiento de datos...",
-                "technologies": "Linux, Windows, VMware ...",
-            }
-        ]
-    },
-    //   -  - Formación - - -
-    "education": {
-        "name": "Formación",
-        "docs": [
-            {
-                "duration": "2020 - 2021",
-                "name": "Técnico Superior en Desarrollo de Aplicaciones Multiplataforma.",
-            },
-            {
-                "duration": "2020 - 2020",
-                "name": "Curso: Imatia, Campus dual, Desarrollador Full Stack (3 meses).",
-            },
-            {
-                "duration": "2017 - 2019",
-                "name": "Técnico Superior en Desarrollo de Aplicaciones Web.",
-            },
-            {
-                "duration": "2014 - 2016",
-                "name": "Técnico Superior en Sistemas Informáticos en Red.",
-            }
-        ]
-    },
-    //   -  - skills  - - -
-    "skills": {
-        "name": "habilidades",
-        "docs": [
-          {
-            "name": "Database",
-            "nameIco":"fa-solid fa-database",
-            "items": [
-              {
-                "name": "PostGres",
-                "ratio": "80"
-              },
-              {
-                "name": "MONGODB",
-                "ratio": "70"
-              },
-              {
-                "name": "MSQL",
-                "ratio": "40"
-              }
-            ]
-          },
-          {
-            "name": "Web",
-            "nameIco":"fa-solid fa-desktop",
-            "items": [
-              {
-                "name": "React, NextJS , Vercel , NodeJs",
-                "ratio": "80"
-              },
-              {
-                "name": "Java, Spring",
-                "ratio": "50"
-              },
-              {
-                "name": "Angular, NodeJs",
-                "ratio": "30"
-              }
-            ]
-          },
-          {
-            "name": "Mobile",
-            "nameIco":"fa-solid fa-mobile",
-            "items": [
-              {
-                "name": "React Native, Expo , NodeJs",
-                "ratio": "80"
-              },
-              {
-                "name": "Java, Kotlin",
-                "ratio": "70"
-              },
-              {
-                "name": "Swift",
-                "ratio": "30"
-              }
-            ]
-          },
-          {
-            "name": "Maquetacion",
-            "nameIco":"fa-solid fa-ruler-combined",
-            "items": [
-              {
-                "name": "HML JS CSS",
-                "ratio": "80"
-              },
-              {
-                "name": "Tailwind",
-                "ratio": "70"
-              },
-              {
-                "name": "Sass",
-                "ratio": "30"
-              }
-            ]
-          },
-          {
-            "name": "Environment",
-            "nameIco":"fa-solid fa-screwdriver-wrench",
-            "items": [
-              {
-                "name": "Git",
-                "ratio": "90"
-              },
-              {
-                "name": "Docker",
-                "ratio": "80"
-              },
-              {
-                "name": "Grafql , Api rest",
-                "ratio": "80"
-              }
-            ]
-          }
-        ]
+  aboutMe: {
+    name: "Conóceme",
+    description:
+      "Desarrollador de aplicaciones web y móvil.<br/>Con formación en Front y Back, capacidad de trabajo en grupo mediante el uso de tecnologías ágiles.<br/>Adaptabilidad a diferentes lenguajes de programación y arquitecturas de diseño.",
+    descAccent:
+      "Estaría encantado de formar parte de su empresa, y seguir desarrollándome en el ámbito profesional.",
+    cvHref: "CV_Bieito.pdf",
+    downloadMSN: "Descarga mi CV",
+  },
+  //   -  - Experiencia - - -
+  experience: {
+    name: "Experiencia",
+    docs: [
+      {
+        year: "2022",
+        duration: "1 año",
+        name: "Galvintec (Diciembre 2021 - Diciembre 2022)",
+        job: "Desarrollador Aplicaciones Android.",
+        performance:
+          "Maquetar y crear funcionalidades a medida en entornos Android.",
+        technologies: "Android Studio, Kotlin, ReactNative…",
       },
-      "onlineCourses":{
-        "name": "¡En constante formación!",
-        "docs": [
+      {
+        year: "2021",
+        duration: "3 meses",
+        name: "Galvintec (Septiembre 2021 - Diciembre 2021)",
+        job: "Desarrollador Aplicaciones Web.",
+        performance: "Creación web e-commerce.",
+        technologies: "NextJS, React, Nodejs…",
+      },
+      {
+        year: "2021",
+        duration: "6 meses",
+        name: "IMATIA (Septiembre 2020 - Marzo 2021)",
+        job: "Desarrollador Web.",
+        performance: "Creación de una web.",
+        technologies: "SQL, Java, Spring, Angular…",
+      },
+      {
+        year: "2019",
+        duration: "3 meses",
+        name: "TOKAPP SCHOOL (Septiembre 2019 - Diciembre 2019)",
+        job: "Desarrollador Web.  (Prácticas) ",
+        performance: "Creación de nuevas funcionalidades para web.",
+        technologies: "PHP, JavaScript, SQL, WordPress, Moodle...",
+      },
+      {
+        year: "2018",
+        duration: "6 meses",
+        name: "LIDERA KNOWLEDGE SL (Diciembre 2018 - Mayo 2019)",
+        job: "Técnico de Sistemas Informáticos en red.",
+        performance:
+          "Creación y mantenimiento de un servidor web, destinado al E-Learning",
+        technologies: "CSM,  WordPress, Moodle",
+      },
+      {
+        year: "2017",
+        duration: "6 meses",
+        name: "SATEC (Marzo 2017 - Agosto 2017)",
+        job: "Gestor de integración",
+        performance:
+          "Configuración, gestión y asistencia de incidencias para rúters.",
+        technologies:
+          "Instalacion y configuración en remoto de Terminales (Cisco, Huawei, Teldat...), Gestion de servicios (VPN, firewall...)",
+      },
+      {
+        year: "2016",
+        duration: "3 meses",
+        name: "SERESCO (Septiembre 2016 - Diciembre 2016)",
+        job: "Técnico de Sistemas Informáticos en red. (Practicas)",
+        performance:
+          "Gestión y mantenimiento de equipos en remoto (seguridad informática, asesoramiento de las infraestructuras informáticas, prevención de riesgos informáticos, copias de seguridad y tratamiento de datos...",
+        technologies: "Linux, Windows, VMware ...",
+      },
+    ],
+  },
+  //   -  - Formación - - -
+  education: {
+    name: "Formación",
+    docs: [
+      {
+        duration: "2020 - 2021",
+        name: "Técnico Superior en Desarrollo de Aplicaciones Multiplataforma.",
+      },
+      {
+        duration: "2020 - 2020",
+        name: "Curso: Imatia, Campus dual, Desarrollador Full Stack (3 meses).",
+      },
+      {
+        duration: "2017 - 2019",
+        name: "Técnico Superior en Desarrollo de Aplicaciones Web.",
+      },
+      {
+        duration: "2014 - 2016",
+        name: "Técnico Superior en Sistemas Informáticos en Red.",
+      },
+    ],
+  },
+  //   -  - skills  - - -
+  skills: {
+    name: "habilidades",
+    docs: [
+      {
+        name: "Database",
+        nameIco: "fa-solid fa-database",
+        items: [
           {
-            "src": "UC-SQL_BASIC.jpg",
-            "alt": "0_UC_SQL_BASIC"
+            name: "PostGres",
+            ratio: "80",
           },
-      
           {
-            "src": "UC-SQL_ADVANCED.jpg",
-            "alt": "1_UC-SQL_COMPLETO"
+            name: "MONGODB",
+            ratio: "70",
           },
-      
           {
-            "src": "UC-POO_JAVA.jpg",
-            "alt": "2_UC-POO_JAVA"
+            name: "MSQL",
+            ratio: "40",
           },
-      
+        ],
+      },
+      {
+        name: "Web",
+        nameIco: "fa-solid fa-desktop",
+        items: [
           {
-            "src": "UC-SPRING_BOOT.jpg",
-            "alt": "3_UC-SPRING_BOOT"
+            name: "React, NextJS , Vercel , NodeJs",
+            ratio: "80",
           },
-      
           {
-            "src": "UC-ANGULAR.jpg",
-            "alt": "4_UC-ANGULAR"
+            name: "Java, Spring",
+            ratio: "50",
           },
-      
           {
-            "src": "UC-MAVEN.jpg",
-            "alt": "5-UC-MAVEN"
+            name: "Angular, NodeJs",
+            ratio: "30",
           },
-      
+        ],
+      },
+      {
+        name: "Mobile",
+        nameIco: "fa-solid fa-mobile",
+        items: [
           {
-            "src": "UC-ECLIPSE.jpg",
-            "alt": "6_UC-ECLIPSE"
+            name: "React Native, Expo , NodeJs",
+            ratio: "80",
           },
-      
           {
-            "src": "UC-EXCEL.jpg",
-            "alt": "7_UC-EXCEL"
+            name: "Java, Kotlin",
+            ratio: "70",
           },
-      
           {
-            "src": "UC-ANDROID.jpg",
-            "alt": "8_UC-ANDROID"
+            name: "Swift",
+            ratio: "30",
           },
-      
+        ],
+      },
+      {
+        name: "Maquetacion",
+        nameIco: "fa-solid fa-ruler-combined",
+        items: [
           {
-            "src": "UC-GIT_BASIC.jpg",
-            "alt": "9_UC-GIT_BASIC"
+            name: "HML JS CSS",
+            ratio: "80",
           },
-      
           {
-            "src": "UC-GIT_ADVANCED.jpg",
-            "alt": "10_UC-ADVANCED"
-          }
-        ]
-      }
-      
-      
-} // end
+            name: "Tailwind",
+            ratio: "70",
+          },
+          {
+            name: "Sass",
+            ratio: "30",
+          },
+        ],
+      },
+      {
+        name: "Environment",
+        nameIco: "fa-solid fa-screwdriver-wrench",
+        items: [
+          {
+            name: "Git",
+            ratio: "90",
+          },
+          {
+            name: "Docker",
+            ratio: "80",
+          },
+          {
+            name: "Grafql , Api rest",
+            ratio: "80",
+          },
+        ],
+      },
+    ],
+  },
+  //   -  - online Courses  - - -
+  onlineCourses: {
+    name: "¡En constante formación!",
+    docs: [
+      {
+        src: "UC-SQL_BASIC.jpg",
+        alt: "0_UC_SQL_BASIC",
+      },
+
+      {
+        src: "UC-SQL_ADVANCED.jpg",
+        alt: "1_UC-SQL_COMPLETO",
+      },
+
+      {
+        src: "UC-POO_JAVA.jpg",
+        alt: "2_UC-POO_JAVA",
+      },
+
+      {
+        src: "UC-SPRING_BOOT.jpg",
+        alt: "3_UC-SPRING_BOOT",
+      },
+
+      {
+        src: "UC-ANGULAR.jpg",
+        alt: "4_UC-ANGULAR",
+      },
+
+      {
+        src: "UC-MAVEN.jpg",
+        alt: "5-UC-MAVEN",
+      },
+
+      {
+        src: "UC-ECLIPSE.jpg",
+        alt: "6_UC-ECLIPSE",
+      },
+
+      {
+        src: "UC-EXCEL.jpg",
+        alt: "7_UC-EXCEL",
+      },
+
+      {
+        src: "UC-ANDROID.jpg",
+        alt: "8_UC-ANDROID",
+      },
+
+      {
+        src: "UC-GIT_BASIC.jpg",
+        alt: "9_UC-GIT_BASIC",
+      },
+
+      {
+        src: "UC-GIT_ADVANCED.jpg",
+        alt: "10_UC-ADVANCED",
+      },
+    ],
+  },
+  //   -  - Proyects  - - -
+  Proyects: {
+    name: "Proyects",
+    docs: [
+      {
+        name: "LaSal",
+        icon: "https://play-lh.googleusercontent.com/vjkJ4vkvVvhl…QJHPrFxlaCwGv47QWdoSpS3806y_YhC8Y3xQ=w240-h480-rw",
+        entity: "Galvintec",
+        tags: "E-commerce",
+        url: "https://play.google.com/store/apps/details?id=com.galvintec.prestapp.lasal&gl=ES",
+        images: [
+          {
+            src: "https://play-lh.googleusercontent.com/ntCPREzfGwvqyKk9UM7RvD1hkQqiIiGrK-BcSTXUqwqULr2C9IjbbbGgsk91jWICTQ8=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/ntCPREzfGwvqyKk9UM7RvD1hkQqiIiGrK-BcSTXUqwqULr2C9IjbbbGgsk91jWICTQ8=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/oAKlFEqLrmdJ7qH41OIMy85RgU3awXoRLjW5QQW9Pp6PaTGXaIdEPHZmHl6i4WWFUtrm=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/oAKlFEqLrmdJ7qH41OIMy85RgU3awXoRLjW5QQW9Pp6PaTGXaIdEPHZmHl6i4WWFUtrm=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/Ebwyu4TE9OfK86Jbuf7OqHdNZI8j-E4NxzBZP42IckrODa9AXQsyz5vZAguPd3jUNpkD=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/Ebwyu4TE9OfK86Jbuf7OqHdNZI8j-E4NxzBZP42IckrODa9AXQsyz5vZAguPd3jUNpkD=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/bTC4JbCquNfRc1eCx3uKBy01wx_tl-5oLUkBMFSt-3vRGyXmhEdoldiPhei1MlGiVN8e=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/bTC4JbCquNfRc1eCx3uKBy01wx_tl-5oLUkBMFSt-3vRGyXmhEdoldiPhei1MlGiVN8e=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/Ax22Rm-h7J0FcaTtlvQhDuJelr0y0Jv_bOxjCqEzCj_dMrl5afkN4_N5YDY4tAQRo6_o=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/Ax22Rm-h7J0FcaTtlvQhDuJelr0y0Jv_bOxjCqEzCj_dMrl5afkN4_N5YDY4tAQRo6_o=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/Ti28lzjTktFTtJKpMaY9Wmi_K8aB6YeDKRVL646szwUCsHKz2CLzqgxT8sej2rVl9DI=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/Ti28lzjTktFTtJKpMaY9Wmi_K8aB6YeDKRVL646szwUCsHKz2CLzqgxT8sej2rVl9DI=w1052-h592-rw 2x",
+          },
+          {
+            src: "https://play-lh.googleusercontent.com/h2V5__T1t5kdWsthT2nLY7hSU7EuA113OSodmsGpWs7MgP-0aLnFigRdV5arpO6NBC8=w526-h296-rw",
+            srcset:
+              "https://play-lh.googleusercontent.com/h2V5__T1t5kdWsthT2nLY7hSU7EuA113OSodmsGpWs7MgP-0aLnFigRdV5arpO6NBC8=w1052-h592-rw 2x",
+          },
+        ],
+      },
+    ],
+  },
+}; // end
